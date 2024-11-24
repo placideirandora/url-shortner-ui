3bd8fc593b19fea8e8ac6cc4f73da9079390cfb1
Placide IRANDORA <placideirandora@gmail.com>
2024-11-24 18:44:10 +0200

    feat: add overview and user input mask pages

---
+++ src/components/pages/AdminOverview.tsx
@@ -0,0 +1,5 @@
+import Typography from '@mui/material/Typography';
+
+export default function AdminOverview() {
+  return <Typography variant="h4">ADMIN OVERVIEW</Typography>;
+}
---
+++ src/components/pages/UserInputMask.tsx
@@ -0,0 +1,5 @@
+import Typography from '@mui/material/Typography';
+
+export default function UserInputMask() {
+  return <Typography variant="h4">USER INPUT MASK</Typography>;
+}
