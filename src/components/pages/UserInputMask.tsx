35c1860d970a07d75b80d279290537bb393f937f
Placide IRANDORA <placideirandora@gmail.com>
2024-11-24 19:36:44 +0200

    ref: update the user input mask page with the required text fields and buttons

--- src/components/pages/UserInputMask.tsx
+++ src/components/pages/UserInputMask.tsx
@@ -1,5 +1,60 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Divider from '@mui/material/Divider';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 
 export default function UserInputMask() {
-  return <Typography variant="h4">USER INPUT MASK</Typography>;
+  return (
+    <Box
+      sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
+      <Typography variant="h4" sx={{ mb: 6, textAlign: 'center' }}>
+        USER INPUT MASK
+      </Typography>
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '150%',
+          justifyContent: 'center',
+        }}
+      >
+        <TextField
+          id="original-url"
+          label="Original URL"
+          variant="outlined"
+          sx={{ width: '100%' }}
+        />
+        <Button variant="contained" size="large" sx={{ ml: 3 }}>
+          Go
+        </Button>
+      </Box>
+
+      <Divider sx={{ width: '200%', my: 6 }} />
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          width: '150%',
+          justifyContent: 'center',
+        }}
+      >
+        <TextField
+          id="shortened-url"
+          label="Shortened URL"
+          variant="outlined"
+          sx={{ width: '100%' }}
+        />
+        <Button variant="contained" size="large" sx={{ ml: 3 }}>
+          Test
+        </Button>
+        <Button variant="contained" size="large" sx={{ ml: 3 }}>
+          Copy
+        </Button>
+      </Box>
+    </Box>
+  );
 }
